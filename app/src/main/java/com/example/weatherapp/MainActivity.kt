1ef55de76ed34ce1bcabb50aa44831f8fefd8ce5
Jamila <rammv4117@gmail.com>
2023-04-27 21:00:57 +0300

    project

---
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.weatherapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.weatherapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(R.layout.activity_main)
+    }
+}
