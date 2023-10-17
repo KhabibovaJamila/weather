5465b828d88b1498893653ebe1f3110afa87e5ba
Jamila <rammv4117@gmail.com>
2023-10-17 23:04:30 +0300

    fragment

--- app/src/main/java/com/example/weather/MainActivity.kt
+++ app/src/main/java/com/example/weather/MainActivity.kt
@@ -2,42 +2,16 @@ package com.example.weather
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import com.android.volley.Request
-import com.android.volley.toolbox.StringRequest
-import com.android.volley.toolbox.Volley
-import com.example.weather.databinding.ActivityMainBinding
-
-const val API_KEY = "0f0736698dce4ceaae8191553231610&q"
+import com.example.weather.fragment.MainFragment
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        binding.GET.setOnClickListener {
-            getResult("London")
+        setContentView(R.layout.activity_main)
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.place, MainFragment.newInstance())
+            .commit()
 
-        }
-
-    }
-    private fun getResult(name: String) {
-      val  url = "https://api.weatherapi.com/v1/current.json" +
-              "?key=$API_KEY=$name=no"
-        val queue = Volley.newRequestQueue(this)
-        val stringRequest = StringRequest(Request.Method.GET,
-         url,
-            {
-                    response->
-                Log.d("MyLog", "Response $response")
-
-            },
-            {
-                 Log.d("MyLog", "Volley error $it")
-            }
-        )
-        queue.add(stringRequest)
     }
 }
